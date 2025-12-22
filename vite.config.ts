ce4d2a9eecca5355881c67b8a85eabcfe211f56f
sai-suraj143 <narayanama345@gmail.com>
2025-12-22 16:15:57 +0530

    vite-config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Maniteja-Portfolio/",
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
-});
+  // base: '/my-portfolio/',  <-- REMOVE THIS LINE or put comment like this
+})
